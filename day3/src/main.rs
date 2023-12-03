b3c35ebbf09a43fea38e26ae0e206209d51f0fdc
Quan ZHOU <quan@CoreMatter.local>
2023-12-03 19:49:43 +0100

    day3 version 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,31 +1,20 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 use std::fmt::{Display, Formatter};
+
+use regex::Regex;
 
 fn main() {
     let input = include_str!("input.txt");
     println!("{:?}", gear_sum(input));
+    println!("{:?}", gear_sum_v2(input));
 }
 
-#[derive(Debug, PartialEq, Hash, Eq, Clone)]
+#[derive(Debug, Hash, Clone)]
 struct Number {
     number: u32,
     row: usize,
     start: usize,
     end: usize,
-}
-
-#[derive(Debug)]
-struct Grid {
-    grid: Vec<Vec<char>>
-}
-
-impl Display for Grid {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        for row in self.grid.iter() {
-            writeln!(f, "{}", String::from_iter(row))?;
-        }
-        Ok(())
-    }
 }
 
 fn gear_sum(input: &str) -> (u32, u32) {
@@ -67,35 +56,34 @@ fn gear_sum(input: &str) -> (u32, u32) {
     let mut chars: HashMap<(usize, usize), Vec<u32>> = HashMap::new();
     for (row_number, row) in matrix.iter().enumerate() {
         for (index, ch) in row.iter().enumerate() {
-            match ch {
-                '0'..='9' => continue,
-                '.' => continue,
-                _ => {
-                    for diff in [-1, 0, 1] {
-                        if let Some(row) = row_number.checked_add_signed(diff) {
-                            bucket
-                                .entry(row)
+            if ch.is_ascii_digit() || ch == &'.' {
+                continue;
+            }
+            for diff in [-1, 0, 1] {
+                if let Some(row) = row_number.checked_add_signed(diff)
+                {
+                    bucket
+                        .entry(row)
+                        .or_default()
+                        .iter()
+                        .filter(|n| {
+                            let start = n.start as i32;
+                            let end = n.end as i32;
+                            start.max(index as i32 - 1)
+                                <= end.min(index as i32 + 1)
+                        })
+                        .for_each(|n| {
+                            chars
+                                .entry((row_number, index))
                                 .or_default()
-                                .iter()
-                                .filter(|n| {
-                                    let start = n.start as i32;
-                                    let end = n.end as i32;
-                                    start.max(index as i32 - 1) <= end.min(index as i32 + 1)
-                                })
-                                .for_each(|n| {
-                                    chars.entry((row_number, index)).or_default().push(n.number);
-                                })
-                        }
-                    }
+                                .push(n.number);
+                        })
                 }
             }
         }
     }
 
-    let part1 = chars
-        .values()
-        .map(|v| v.iter().sum::<u32>())
-        .sum();
+    let part1 = chars.values().map(|v| v.iter().sum::<u32>()).sum();
 
     let part2 = chars
         .values()
@@ -106,13 +94,112 @@ fn gear_sum(input: &str) -> (u32, u32) {
     (part1, part2)
 }
 
+type CharPos = (usize, usize);
+
+#[derive(Debug)]
+struct Grid<'a> {
+    grid: Vec<&'a str>,
+}
+
+impl<'a> Display for Grid<'a> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        for row in self.grid.iter() {
+            writeln!(f, "{}", row)?;
+        }
+        Ok(())
+    }
+}
+impl<'a> Grid<'a> {
+    fn get_numbers_bucket(&self) -> HashMap<usize, Vec<Number>> {
+        let num_regex = Regex::new(r"\d+").unwrap();
+        self.grid
+            .iter()
+            .enumerate()
+            .map(|(r, line)| {
+                num_regex
+                    .find_iter(line)
+                    .map(move |m| {
+                        (
+                            r,
+                            Number {
+                                number: m.as_str().parse().unwrap(),
+                                start: m.start(),
+                                end: m.end() - 1,
+                                row: r,
+                            },
+                        )
+                    })
+                    .collect::<Vec<(usize, Number)>>()
+            })
+            .fold(HashMap::new(), |mut acc, v| {
+                v.iter().for_each(|(r, n)| {
+                    acc.entry(*r).or_default().push(n.clone())
+                });
+                acc
+            })
+    }
+
+    fn get_adjacent(
+        &self,
+        bucket: &HashMap<usize, Vec<Number>>,
+    ) -> HashMap<CharPos, Vec<u32>> {
+        let mut chars: HashMap<CharPos, Vec<u32>> = HashMap::new();
+        self.grid.iter().enumerate().for_each(|(r, line)| {
+            line.chars()
+                .enumerate()
+                .filter(|(_, ch)| !ch.is_ascii_digit() && ch != &'.')
+                .for_each(|(c, _)| {
+                    for diff in [-1, 0, 1] {
+                        if let Some(row) = r.checked_add_signed(diff)
+                        {
+                            bucket
+                                .get(&row)
+                                .unwrap_or(&vec![])
+                                .iter()
+                                .filter(|n| {
+                                    let c = c as i32;
+                                    (c - 1).max(n.start as i32)
+                                        <= (c + 1).min(n.end as i32)
+                                })
+                                .for_each(|n| {
+                                    chars
+                                        .entry((r, c))
+                                        .or_default()
+                                        .push(n.number);
+                                });
+                        }
+                    }
+                })
+        });
+        chars
+    }
+}
+
+fn gear_sum_v2(input: &str) -> (u32, u32) {
+    let grid = Grid {
+        grid: input.lines().collect(),
+    };
+    let bucket = grid.get_numbers_bucket();
+    let adjacent = grid.get_adjacent(&bucket);
+
+    (
+        adjacent.values().map(|v| v.iter().sum::<u32>()).sum(),
+        adjacent
+            .values()
+            .filter(|v| v.len() == 2)
+            .map(|v| v.iter().product::<u32>())
+            .sum(),
+    )
+}
+
 #[cfg(test)]
 mod tests {
-    use super::*;
     use indoc::indoc;
 
+    use super::*;
+
     #[test]
-    fn test_main() {
+    fn test_gear_sum() {
         let input = indoc! {
             r#"
             467..114..
@@ -128,5 +215,24 @@ mod tests {
             "#
         };
         assert_eq!((4361, 467835), gear_sum(input))
+    }
+
+    #[test]
+    fn test_gear_sum_v2() {
+        let input = indoc! {
+            r#"
+            467..114..
+            ...*......
+            ..35..633.
+            ......#...
+            617*......
+            .....+.58.
+            ..592.....
+            ......755.
+            ...$.*....
+            .664.598..
+            "#
+        };
+        assert_eq!((4361, 467835), gear_sum_v2(input))
     }
 }
